8d68eab2b29257dec1c9c4b8c61cde51bac16538
AnshLN <anshparmar1502@gmail.com>
2024-07-22 11:06:39 +0530

    saved

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://devstore-bk9e.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
